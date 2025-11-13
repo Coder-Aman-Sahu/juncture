6c9020491495b21275e5309e5b119e7629bc9bb9
SahuAman03 <sahuaman212@gmail.com>
2025-11-13 20:59:24 +0530

    Initial project setup with frontend and backend

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import "../App.css";
+export default function LandingPage() {
+    const router = useNavigate();
+    return (
+        <div className="landingPageContainer">
+            <nav>
+                <div className="navHeader">
+                    <h2>Juncture</h2>
+                </div>
+                <div className="navlist">
+                    <p>Join as Guest</p>
+                    <p>Register</p>
+                    <div role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+
+            <div className="landingMainContainer">
+                <div >
+                    <h1><span style={{color:"#FF9839"}}>Connect</span> with your loved Ones</h1>
+                
+                    <p>Cover a distance by Juncture</p>
+                    <div role='button'>
+                        <Link to={"/auth"}>Get Started</Link>
+                    </div>
+                </div>
+                <div >
+
+                    <img src="/mobile.png" alt=""></img>
+                </div>
+            </div>
+
+        </div>
+    )
+}
